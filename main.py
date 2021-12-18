a6ca20195f72809a231b7cdb90b8fbc3143643b4
Enes <karagoz.enes@outlook.com>
2021-12-19 01:33:42 +0300

    Cesar's Algorithm & Dockerfile

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+alphabet = [chr(i) for i in range(32,127)]
+
 def encrypt(plain, key):
     # type: (str, int) -> str
 
     # TODO your code will be here
-    # this is new comment
+    encrypted_text = ""
+    key = key % len(alphabet)
+    for i in plain:
+        if key + alphabet.index(i) >= len(alphabet):
+            new_index = key + alphabet.index(i) - len(alphabet)
+        else:
+            new_index = key + alphabet.index(i)
+        encrypted_text += alphabet[new_index]
+    return encrypted_text
     pass
 
 
@@ -10,6 +20,15 @@ def decrypt(encrypted, key):
     # type: (str, int) -> str
 
     # TODO your code will be here
+    decrypted_text = ""
+    key = key % len(alphabet)
+    for i in encrypted:
+        if key > alphabet.index(i):
+            new_index = len(alphabet) + alphabet.index(i) - key
+        else:
+            new_index = alphabet.index(i) - key
+        decrypted_text += alphabet[new_index]
+    return decrypted_text
     pass
 
 
